52675505d278720f2ae1e4765d2d1aebd331f8ba
Ihor Shkurko <ishkurko@gmail.com>
2018-05-17 12:31:55 +0300

    Create pg_create_db.sh

---
+++ assets/pg_create_db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE uaa OWNER postgres;
+    CREATE DATABASE dashboard OWNER postgres;
+    CREATE DATABASE entity OWNER postgres;
+    CREATE DATABASE balance OWNER postgres;
+EOSQL

b0110176c813c24267a6cafac9659df122559030
somelchuk <somelchuk@icthh.com>
2019-05-06 17:37:59 +0300

    update timeline, elasticksearch, kafka, zookeper

--- assets/pg_create_db.sh
+++ assets/pg_create_db.sh
@@ -5,4 +5,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE dashboard OWNER postgres;
     CREATE DATABASE entity OWNER postgres;
     CREATE DATABASE balance OWNER postgres;
+    CREATE DATABASE timeline OWNER postgres;
 EOSQL

20642bec4246750eb30016f3db6aaf963eb713e3
Andrew Kikot <andrew.kikot@gmail.com>
2019-05-17 11:49:37 +0300

    update if value only

--- src/app/shared/jsf-extention/widgets/ext-select/ext-select.component.ts
+++ src/app/shared/jsf-extention/widgets/ext-select/ext-select.component.ts
@@ -118,7 +118,7 @@ export class ExtSelectComponent implements OnInit, OnDestroy, AfterViewInit {
             .subscribe(() => {
                 this.filterElements();
             });
-        this.jsf.updateValue(this, this.controlValue);
+        if (this.controlValue) {this.jsf.updateValue(this, this.controlValue)}
     }
 
     private filterElements() {

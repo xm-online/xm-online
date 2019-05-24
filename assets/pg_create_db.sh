eeffa7db97fc3c07dc446ea51a07b41570697a9d
Yuriy Kruchko <yuriy.kruchko@jevera.software>
2019-05-24 08:17:58 -0400

    add creation scheams on db and store pgdata and elastic search data to volumes

--- assets/pg_create_db.sh
+++ assets/pg_create_db.sh
@@ -2,8 +2,18 @@
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE uaa OWNER postgres;
+    \connect uaa;
+    CREATE SCHEMA IF NOT EXISTS xm;
     CREATE DATABASE dashboard OWNER postgres;
+    \connect dashboard;
+    CREATE SCHEMA IF NOT EXISTS xm;
     CREATE DATABASE entity OWNER postgres;
+    \connect entity; 
+    CREATE SCHEMA IF NOT EXISTS xm;
     CREATE DATABASE balance OWNER postgres;
+    \connect balance;
+    CREATE SCHEMA IF NOT EXISTS xm;
     CREATE DATABASE timeline OWNER postgres;
+    \connect timeline;
+    CREATE SCHEMA IF NOT EXISTS xm;
 EOSQL

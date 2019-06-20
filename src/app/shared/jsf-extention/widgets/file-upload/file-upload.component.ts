5a4111126dfd0eca79cec15ea272ae25ee282ab7
Andrew Kikot <andrew.kikot@gmail.com>
2019-06-20 14:51:29 +0300

    adding widget to handle upload file to cloud service

--- src/app/shared/jsf-extention/widgets/file-upload/file-upload.component.ts
+++ src/app/shared/jsf-extention/widgets/file-upload/file-upload.component.ts
@@ -1,44 +1,62 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
 import { JsonSchemaFormService } from 'angular2-json-schema-form';
+import { finalize } from 'rxjs/operators';
 
 @Component({
     selector: 'xm-ajfs-file-upload',
-    templateUrl: 'file-upload.component.html'
+    templateUrl: 'file-upload.component.html',
+    styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent implements OnInit {
 
     options: any;
     controlValue: any;
+    uploading = false;
+    file: any;
     @Input() layoutNode: any;
 
-    constructor(private jsf: JsonSchemaFormService, private httpClient: HttpClient) {
+    constructor(private jsf: JsonSchemaFormService,
+                private httpClient: HttpClient) {
     }
 
     ngOnInit() {
         this.options = this.layoutNode.options || {};
         this.jsf.initializeControl(this);
+
     }
 
-
-    updateValue(event) {
+    updateFile(event) {
         if (event.target.files.length > 0) {
-            const file = event.target.files[0];
-            const formData: FormData = new FormData();
-            const headers: HttpHeaders = new HttpHeaders();
-            formData.append('file', file, file.name);
-            this.saveFile(formData, headers, file.name);
+            this.file = event.target.files[0];
         }
     }
 
-    private saveFile(formData: FormData, headers: HttpHeaders, name: string) {
+    uploadFile(fileData: any) {
+        if (!fileData) {return false}
+        const file = fileData;
+        const formData: FormData = new FormData();
+        const headers: HttpHeaders = new HttpHeaders();
+        formData.append('file', file, file.name);
+        this.saveFile(formData, headers);
+    }
+
+    resetFile() {
+        this.file = null;
+        this.jsf.updateValue(this, null);
+    }
+
+    private saveFile(formData: FormData, headers: HttpHeaders) {
         const apiUrl = this.options['url'] || null;
         if (apiUrl) {
+            this.uploading = true;
             this.httpClient
                 .post(this.options['url'], formData, { headers: headers})
+                .pipe(
+                    finalize(() => this.uploading = false)
+                )
                 .subscribe(res => {
-                    this.jsf.updateValue(this, name);
+                    this.jsf.updateValue(this, res.data.key);
                 });
         }
     }

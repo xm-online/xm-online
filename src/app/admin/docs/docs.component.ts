0d57a486a6b0896f56438dc1fc2fd3a34c16fe4f
Andrew Kikot <andrew.kikot@gmail.com>
2019-07-09 10:24:42 +0300

    adding swager with component itself and not like iframe

--- src/app/admin/docs/docs.component.ts
+++ src/app/admin/docs/docs.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import SwaggerUI from 'swagger-ui'
+
 
 @Component({
     selector: 'xm-docs',
     templateUrl: './docs.component.html'
 })
-export class JhiDocsComponent {
+export class JhiDocsComponent implements OnInit, AfterViewInit {
     constructor(
+        private http: HttpClient
     ) {
+    }
+
+    ngOnInit() {
+
+    }
+
+    ngAfterViewInit() {
+        this.http.get('/swagger-resources').subscribe((data) => {
+            console.log(data);
+            SwaggerUI({
+                dom_id: '#swagerTEMP',
+                supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
+                url: '/v2/api-docs',
+                docExpansion: 'none'
+            });
+        });
     }
 }

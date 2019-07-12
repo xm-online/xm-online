d20cab05933ef823975c190c2707817b386e33e1
Andrew Kikot <andrew.kikot@gmail.com>
2019-07-12 21:46:57 +0300

    get config and seeding in select

--- src/app/admin/docs/docs.component.ts
+++ src/app/admin/docs/docs.component.ts
@@ -3,30 +3,42 @@ import { HttpClient } from '@angular/common/http';
 
 import SwaggerUI from 'swagger-ui'
 
-
 @Component({
     selector: 'xm-docs',
     templateUrl: './docs.component.html'
 })
 export class JhiDocsComponent implements OnInit, AfterViewInit {
+
+    swaggerResources: any[];
+    currentResource: any;
+
     constructor(
         private http: HttpClient
     ) {
     }
 
     ngOnInit() {
-
     }
 
     ngAfterViewInit() {
-        this.http.get('/swagger-resources').subscribe((data) => {
-            console.log(data);
-            SwaggerUI({
-                dom_id: '#swaggerHolder',
-                supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
-                url: '/v2/api-docs',
-                docExpansion: 'none'
+        this.http
+            .get('/swagger-resources')
+            .subscribe((data: any[]) => {
+                this.swaggerResources = data;
+                this.currentResource = this.swaggerResources[0].location;
+                this.updateSwagger(this.currentResource);
             });
+    }
+
+    updateSwagger(resource) {
+        console.log(resource);
+        SwaggerUI({
+            dom_id: '#swaggerHolder',
+            supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
+            url: resource,
+            docExpansion: 'none',
+            apisSorter: 'alpha',
+            showRequestHeaders: false
         });
     }
 }

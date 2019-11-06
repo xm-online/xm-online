dc750c49fe11352dbf10cd426d6792e59915422d
ozelenyi <oleksandr.zelenyi@jevera.software>
2019-11-06 16:05:52 +0200

    Core. Link-detail-search-section. Replaced link spec icon with entity spec icon

--- src/app/xm-entity/link-detail-dialog/link-detail-search-section.component.ts
+++ src/app/xm-entity/link-detail-dialog/link-detail-search-section.component.ts
@@ -6,9 +6,11 @@ import { JhiEventManager } from 'ng-jhipster';
 import { filter, map } from 'rxjs/operators';
 
 import { XmConfigService } from '../../shared';
+import { XmEntitySpec } from '../index';
 import { LinkSpec } from '../shared/link-spec.model';
 import { Link } from '../shared/link.model';
 import { LinkService } from '../shared/link.service';
+import { Spec } from '../shared/spec.model';
 import { XmEntity } from '../shared/xm-entity.model';
 import { XmEntityService } from '../shared/xm-entity.service';
 
@@ -21,16 +23,17 @@ declare let swal: any;
 })
 export class LinkDetailSearchSectionComponent implements OnInit {
 
-    @Input() linkSpec: LinkSpec;
-    @Input() sourceXmEntity: XmEntity;
+    @Input() public linkSpec: LinkSpec;
+    @Input() public sourceXmEntity: XmEntity;
+    @Input() public spec: Spec;
 
-    xmEntity: XmEntity = {};
-    searchQuery: string;
-    searchXmEntities: XmEntity[];
-    page: number;
-    total: number;
-    showLoader: boolean;
-    linkSpecQuery: any;
+    public xmEntity: XmEntity = {};
+    public searchQuery: string;
+    public searchXmEntities: XmEntity[];
+    public total: number;
+    public showLoader: boolean;
+    private page: number;
+    private linkSpecQuery: any;
 
     constructor(private activeModal: NgbActiveModal,
                 private xmEntityService: XmEntityService,
@@ -40,51 +43,25 @@ export class LinkDetailSearchSectionComponent implements OnInit {
                 private configService: XmConfigService) {
     }
 
-    ngOnInit() {
+    public ngOnInit(): void {
         this.getLinkSpecConfig().subscribe((res) => this.linkSpecQuery = res || null);
         this.onSearch();
     }
 
-    getLinkSpecConfig() {
-       return this.configService.getUiConfig().pipe(
-           map((res) => res.applications.config.entities.find((entity) => entity.typeKey === this.sourceXmEntity.typeKey) || {}),
-           filter((entity) => entity.hasOwnProperty('links')),
-           map((entity) => entity.links.find((link) => link.key === this.linkSpec.key)['filterQuery']),
-        );
+    public getEntitySpec(typeKey: string): XmEntitySpec {
+        return this.spec.types.filter( (t) => t.key === typeKey).shift();
     }
 
-    onSearch() {
-        this.searchXmEntities = [];
-        this.page = 0;
-        this.load();
-    }
-
-    onShowMore() {
+    public onShowMore(): void {
         this.page++;
         this.load();
     }
 
-    load() {
-        this.showLoader = true;
-        this.xmEntityService.search({
-            query: `(typeKey:${this.linkSpec.typeKey}* OR typeKey:${this.linkSpec.typeKey})`
-                + (this.linkSpecQuery ? ` AND ${this.linkSpecQuery}` : '')
-                + (this.searchQuery ? ` AND ${this.searchQuery}` : ''),
-            size: 5,
-            page: this.page
-        }).subscribe((xmEntities: HttpResponse<XmEntity[]>) => {
-                this.total = parseInt(xmEntities.headers.get('X-Total-Count'), 10);
-                this.searchXmEntities.push(...xmEntities.body);
-            },
-            (err) => console.log(err), // tslint:disable-line
-            () => this.showLoader = false);
-    }
-
-    onCancel() {
+    public onCancel(): void {
         this.activeModal.dismiss('cancel');
     }
 
-    onAdd(targetXmEntity: XmEntity) {
+    public onAdd(targetXmEntity: XmEntity): void {
         this.showLoader = true;
         const link: Link = {};
         link.target = targetXmEntity;
@@ -100,6 +77,37 @@ export class LinkDetailSearchSectionComponent implements OnInit {
                 this.showLoader = false;
             },
             () => this.activeModal.dismiss(true));
+    }
+
+    private load(): void {
+        this.showLoader = true;
+        this.xmEntityService.search({
+            query: `(typeKey:${this.linkSpec.typeKey}* OR typeKey:${this.linkSpec.typeKey})`
+                + (this.linkSpecQuery ? ` AND ${this.linkSpecQuery}` : '')
+                + (this.searchQuery ? ` AND ${this.searchQuery}` : ''),
+            size: 5,
+            page: this.page,
+        }).subscribe((xmEntities: HttpResponse<XmEntity[]>) => {
+                this.total = parseInt(xmEntities.headers.get('X-Total-Count'), 10);
+                this.searchXmEntities.push(...xmEntities.body);
+            },
+            (err) => console.log(err), // tslint:disable-line
+            () => this.showLoader = false);
+    }
+
+    private getLinkSpecConfig(): any {
+        return this.configService.getUiConfig().pipe(
+            map((res) => res.applications.config.entities
+                .find((entity) => entity.typeKey === this.sourceXmEntity.typeKey) || {}),
+            filter((entity) => entity.hasOwnProperty('links')),
+            map((entity) => entity.links.find((link) => link.key === this.linkSpec.key)['filterQuery']),
+        );
+    }
+
+    private onSearch(): void {
+        this.searchXmEntities = [];
+        this.page = 0;
+        this.load();
     }
 
     private alert(type: string, key: string): void {

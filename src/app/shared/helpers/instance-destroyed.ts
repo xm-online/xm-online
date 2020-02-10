780d8c3930f73867359c2d28fa0dc3301b759c93
ssenko <sergeysenja1992@gmail.com>
2020-02-10 12:23:19 +0200

    fix bugs from previos commiter

--- src/app/shared/helpers/instance-destroyed.ts
+++ src/app/shared/helpers/instance-destroyed.ts
@@ -1,10 +1,12 @@
-/* tslint:disable:unbound-method */
 import { OnDestroy } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
 
+
 export function instanceDestroyed(instance: OnDestroy): Observable<void> {
+    // eslint-disable-next-line @typescript-eslint/unbound-method
     const oldNgOnDestroy = instance.ngOnDestroy;
     const stop$ = new Subject<void>();
+    // eslint-disable-next-line @typescript-eslint/unbound-method
     instance.ngOnDestroy = () => {
         oldNgOnDestroy.apply(instance);
         stop$.next();

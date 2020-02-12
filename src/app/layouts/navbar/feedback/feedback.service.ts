68aa2b6f7189dae5335914204aba53554850a0e5
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-02-12 11:51:00 +0200

    fix(feetback): add attributes

--- src/app/layouts/navbar/feedback/feedback.service.ts
+++ src/app/layouts/navbar/feedback/feedback.service.ts
@@ -15,7 +15,7 @@ export class FeedbackService {
     constructor(private httpClient: HttpClient) { }
 
     /** @todo: url should be passed from configs */
-    public create(data: IFeedbackRequest, url: string = '/entity/api/functions/FEEDBACK'): Observable<unknown> {
-        return this.httpClient.post(url, data);
+    public create(attributes: IFeedbackRequest, url: string = '/entity/api/functions/FEEDBACK'): Observable<unknown> {
+        return this.httpClient.post(url, {attributes});
     }
 }

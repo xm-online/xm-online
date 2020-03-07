2a60fca43b46e622c2a8dc06907d929f2bd01baa
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-03-07 20:05:52 +0200

    feat(tsconfig): provide main modules

---
+++ src/app/shared/auth/index.ts
@@ -0,0 +1,9 @@
+export { AccountService } from './account.service';
+export * from './auth.constants';
+export { AuthService } from './auth.service';
+export * from './auth-jwt.service';
+export { CSRFService } from './csrf.service';
+export { HasAnyAuthorityDirective } from './has-any-authority.directive';
+export { Principal } from './principal.service';
+export * from './state-storage.service';
+export { UserRouteAccessService } from './user-route-access-service';

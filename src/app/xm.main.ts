4b5c92b9992dfc4a3eee034d46c7a8a95a231325
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-03-29 13:58:38 +0300

    chore(xm-main): remove xm-main

--- src/app/xm.main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { ProdConfig } from './blocks/config/prod.config';
-import { XmModule } from './xm.module';
-
-ProdConfig();
-
-platformBrowserDynamic().bootstrapModule(XmModule);

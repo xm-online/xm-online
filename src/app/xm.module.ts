6086ce1670dcff759a705f631266ba256df0c873
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-03-31 11:48:01 +0300

    chore: move interceptors to modules

--- src/app/xm.module.ts
+++ src/app/xm.module.ts
@@ -1,24 +1,20 @@
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
-import { APP_INITIALIZER, Injector, NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { UserRouteAccessService } from '@xm-ngx/core/auth';
-import { MarkdownModule } from 'ngx-markdown';
 import { XmAlertModule } from '@xm-ngx/alert';
-import { LocalStorageService, NgxWebstorageModule, SessionStorageService } from 'ngx-webstorage';
-import { PaginationConfig } from './blocks/config/uib-pagination.config';
-import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
-import { AuthInterceptor } from './blocks/interceptor/auth.interceptor';
-import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
-import { ProfileService, XmMainComponent } from './layouts';
-import { LayoutModule } from './layouts/layout.module';
-import { HttpLoaderFactory, XmTranslationModule } from '@xm-ngx/translation';
 
-import { XmApplicationConfigService } from '@xm-ngx/core';
+import { XmApplicationConfigService, XmCoreModule } from '@xm-ngx/core';
+import { UserRouteAccessService } from '@xm-ngx/core/auth';
 import { XmDashboardModule } from '@xm-ngx/dynamic';
+import { HttpLoaderFactory, XmTranslationModule } from '@xm-ngx/translation';
+import { MarkdownModule } from 'ngx-markdown';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+import { XmMainComponent } from './layouts';
+import { LayoutModule } from './layouts/layout.module';
+import { XmCoreAuthModule } from './modules/xm-core-auth/src/xm-core-auth.module';
 import { XmRoutingModule } from './xm-routing.module';
-import { XmCoreModule, XmEventManager } from '@xm-ngx/core';
 
 export function appInitializerFn(appConfig: XmApplicationConfigService): () => Promise<any> {
     // tslint:disable-next-line
@@ -35,10 +31,11 @@ export function appInitializerFn(appConfig: XmApplicationConfigService): () => P
         BrowserAnimationsModule,
         XmRoutingModule,
         XmCoreModule.forRoot(),
+        XmCoreAuthModule.forRoot(),
         NgxWebstorageModule.forRoot({prefix: 'jhi', separator: '-'}),
         TranslateModule.forRoot({
             isolate: false,
-            loader: { deps: [HttpClient], provide: TranslateLoader, useFactory: HttpLoaderFactory },
+            loader: {deps: [HttpClient], provide: TranslateLoader, useFactory: HttpLoaderFactory},
         }),
         XmAlertModule.forRoot(),
         XmTranslationModule.forRoot(),
@@ -54,31 +51,6 @@ export function appInitializerFn(appConfig: XmApplicationConfigService): () => P
             multi: true,
             deps: [XmApplicationConfigService],
         },
-        ProfileService,
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: AuthInterceptor,
-            multi: true,
-            deps: [
-                LocalStorageService,
-                SessionStorageService,
-            ],
-        },
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: AuthExpiredInterceptor,
-            multi: true,
-            deps: [
-                Injector,
-            ],
-        },
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: ErrorHandlerInterceptor,
-            multi: true,
-            deps: [XmEventManager],
-        },
-        PaginationConfig,
         UserRouteAccessService,
     ],
     bootstrap: [XmMainComponent],
